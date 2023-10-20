5f311a110682bba872ae6cd8bab66c92da7ffac4
Youngbin Ryu <ryuyb1105@gmail.com>
2023-10-20 15:06:38 -0700

    first commit

---
+++ test_suite.py
@@ -0,0 +1,23 @@
+import time
+import RPi.GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+#using physical pin 11 to blink an LED
+GPIO.setmode(GPIO.BOARD)
+chan_list = [11]
+GPIO.setup(chan_list, GPIO.OUT)
+#Following commands control the state of the output
+#GPIO.output(pin, GPIO.HIGH)
+#GPIO.output(pin, GPIO.LOW)
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+# get reading from adc 
+# mcp.read_adc(adc_channel)
+
+while True: 
+  time.sleep(0.5) 
+ 
